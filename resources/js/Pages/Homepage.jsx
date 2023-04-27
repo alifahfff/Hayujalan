dedd3b13867eed3eb389625511e5cbba3df12da8
novakensa29 <nova.kensa.tif20@polban.ac.id>
2023-04-27 12:52:12 +0700

    Dashboard

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -1,3 +1,4 @@
+import Status from '@/Components/Homepage/Status';
 import Navbar from '@/Components/Navbar';
 import React from 'react';
 
@@ -6,6 +7,14 @@ export default function Homepage(props) {
     return (
         <div className='min-h-screen bg-slate-50'>
             <Navbar/>
+            <div className='ml-6'>
+                <a>Dashboard</a>
+            </div>
+            <div className='flex justify-around'>
+                <Status/>
+                <Status/>
+                <Status/>
+            </div>
         </div>
     )
 }
