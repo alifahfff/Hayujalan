0b276217b5dd3c50a4f5a871f4ce503756e329ac
novakensa29 <nova.kensa.tif20@polban.ac.id>
2023-04-29 16:16:37 +0700

    Crew

--- resources/js/Components/Item Quotation/Crew.jsx
+++ resources/js/Components/Item Quotation/Crew.jsx
@@ -1,33 +1,53 @@
 const Crew = () => {
     return (
-        <div className="overflow-x-auto">
-            <table className="table w-full" data-theme="bumblebee">
-                <thead>
-                <tr className="bg-blue">
-                    <th>ID</th>
-                    <th>Crew Operasional</th>
-                    <th>Biaya</th>
-                    <th>Status</th>
-                    <th>Aksi</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <th>CO001</th>
-                    <td>Driver 1</td>
-                    <td>Rp.150.000</td>
-                    <td>/day/bus</td>
-                    <td>y</td>
-                </tr>
-                <tr className="active">
-                    <th>CO001</th>
-                    <td>Driver 1</td>
-                    <td>Rp.150.000</td>
-                    <td>/day/bus</td>
-                    <td>y</td>
-                </tr>
-                </tbody>
-            </table>
+        <div className="flex flex-col">
+            <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                <div className="overflow-hidden">
+                    <table className="min-w-full text-left text-sm font-dark">
+                    <thead className="border-b bg-abu font-medium dark:border-neutral-500 dark:bg-neutral-600">
+                        <tr>
+                        <th scope="col" className="px-6 py-4">ID</th>
+                        <th scope="col" className="px-6 py-4">Crew Operasional</th>
+                        <th scope="col" className="px-6 py-4">Biaya</th>
+                        <th scope="col" className="px-6 py-4">Status</th>
+                        <th scope="col" className="px-6 py-4">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="border-b dark:border-neutral-500">
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">CO001</td>
+                        <td className="whitespace-nowrap px-6 py-4">Driver 1</td>
+                        <td className="whitespace-nowrap px-6 py-4">Rp.150.000</td>
+                        <td className="whitespace-nowrap px-6 py-4">/day/bus</td>
+                        <td className="whitespace-nowrap px-6 py-4"></td>
+                        </tr>
+                        <tr className="border-b bg-abu dark:border-neutral-500">
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">CO001</td>
+                        <td className="whitespace-nowrap px-6 py-4">Driver 1</td>
+                        <td className="whitespace-nowrap px-6 py-4">Rp.150.000</td>
+                        <td className="whitespace-nowrap px-6 py-4">/day/bus</td>
+                        <td className="whitespace-nowrap px-6 py-4"></td>
+                        </tr>
+                        <tr className="border-b dark:border-neutral-500">
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">CO001</td>
+                        <td className="whitespace-nowrap px-6 py-4">Driver 1</td>
+                        <td className="whitespace-nowrap px-6 py-4">Rp.150.000</td>
+                        <td className="whitespace-nowrap px-6 py-4">/day/bus</td>
+                        <td className="whitespace-nowrap px-6 py-4"></td>
+                        </tr>
+                        <tr className="border-b bg-abu dark:border-neutral-500">
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">CO001</td>
+                        <td className="whitespace-nowrap px-6 py-4">Driver 1</td>
+                        <td className="whitespace-nowrap px-6 py-4">Rp.150.000</td>
+                        <td className="whitespace-nowrap px-6 py-4">/day/bus</td>
+                        <td className="whitespace-nowrap px-6 py-4"></td>
+                        </tr>
+                    </tbody>
+                    </table>
+                </div>
+                </div>
+            </div>
         </div>
     )
 }
