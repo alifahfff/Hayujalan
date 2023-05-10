dd1d42577d406b80d531dac823bbd8ffe46141eb
Yoga Nizar <nizaryhabib@gmail.com>
2023-05-11 06:12:19 +0700

    revisi nav,dashboard

--- resources/js/Components/Dashboard/Status.jsx
+++ resources/js/Components/Dashboard/Status.jsx
@@ -3,13 +3,13 @@ import React from "react"
 const Status = (status) => {
     console.log(status.status.icon)
     return (
-        <div className= "stats shadow mr-10">
+        <div className= "grow stats shadow mr-10">
             <div className="stat">
                 <div className="flex justify-between">
-                <div className="stat-figure text-4xl w-1/5">{status.status.icon}</div>
-                <div className="stat-title mt-2 ml-2 text-white">{status.status.name}</div>
+                <div className="grow stat-figure text-4xl w-1/5">{status.status.icon}</div>
+                <div className="grow stat-title mt-2 ml-2 text-white">{status.status.name}</div>
             </div>
-            <div className="stat-value ml-2 text-white">{status.status.total}</div>
+            <div className="grow stat-value ml-2 text-white">{status.status.total}</div>
             </div>
         </div>
     )
