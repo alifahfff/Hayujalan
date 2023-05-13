c6c7bacc4f0eb12559c8d761a3bd54b4f67a1f8c
Yoga Nizar <nizaryhabib@gmail.com>
2023-05-14 00:53:45 +0700

    update

--- resources/js/Pages/Quotation/QuotationManual.jsx
+++ resources/js/Pages/Quotation/QuotationManual.jsx
@@ -1,65 +1,62 @@
-import React, { useState } from 'react';
-import { Head } from '@inertiajs/inertia-react';
-import Navbar from '@/Components/Navbar';
-import NewsLists from '@/Components/Homepage/NewsLists';
-import Paginator from '@/Components/Homepage/Paginator';
-import AreaWisata from '@/Components/Vendor/AreaWisata/AreaWisata';
-import Pagination from '@/Components/Pagination';
+import React, { useState } from "react";
+import { Head } from "@inertiajs/inertia-react";
+import Navbar from "@/Components/Navbar";
+import NewsLists from "@/Components/Homepage/NewsLists";
+import Paginator from "@/Components/Homepage/Paginator";
+import AreaWisata from "@/Components/Vendor/AreaWisata/AreaWisata";
+import Pagination from "@/Components/Pagination";
 import { BsPlusSquare } from "react-icons/bs";
-import ModalArea from '@/Components/Vendor/AreaWisata/ModalArea';
+import ModalArea from "@/Components/Vendor/AreaWisata/ModalArea";
 
+export default function VendorArea(props) {
+  const [areaList, setAreaList] = useState([
+    {
+      id: "AW001",
+      area: "Bandung",
+    },
+    {
+      id: "AW002",
+      area: "Jakarta",
+    },
+  ]);
 
-export default function VendorArea (props) {
-    const [areaList, setAreaList] = useState([
-        {
-            id:'AW001',
-            area:'Bandung',    
+  const [showModal, setShowModal] = useState(false);
 
-        },
-        {
-            id:'AW002',
-            area:'Jakarta',    
+  const handleOnClose = () => setShowModal(false);
 
-        },
-    ])
-
-    const [showModal, setShowModal] = useState(false);
-    
-    const handleOnClose = () => setShowModal(false);
-    
   return (
-    <div className='min-h-screen bg-abu'>
-        {/* Navbar */}
-            <Navbar user={props.auth.user} />
-        
-        {/* Content */}
-            <div className='ml-6'>
-                <a>Data Area Wisata</a>
-            </div>
-            <div className='flex justify-between m-6 mt-2 mb-3'>
-                <a className='text-2xl font-bold text-black'>Data Quotation</a>
-                <button 
-                onClick={() => setShowModal(true)}
-                className="btn gap-2 btn-outline rounded-full btn-sm px-5 bg-white hover:bg-gray-100 text-[#C1C0BF]"
-                >
-                Tambah Data | 
-                <BsPlusSquare/>
-                </button>
-            </div>
-            <div className='relative bg-white shadow-xl m-6 mt-3 md:max-xl:flex ring-1 ring-gray-900/5'>
-                <div className='p-4 bg-kuning border-b border-gray-200'></div>
-                <div className='bg-white border-b border-gray-200'>
-                    <div className=''>
-                        <AreaWisata area={areaList}/>
-                        <div className='m-2 flex justify-between items-center'>
-                        <a className='text-[10px] text-black'>Showing 1 - 4 of 10</a>
-                        <Pagination/>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <div className="min-h-screen bg-abu">
+      {/* Navbar */}
+      <Navbar user={props.auth.user} />
 
-            <ModalArea onClose={handleOnClose} visible={showModal}/>
+      {/* Content */}
+      <div className="ml-6">
+        <a>Data Area Wisata</a>
+      </div>
+      <div className="flex justify-between m-6 mt-2 mb-3">
+        <a className="text-2xl font-bold text-black">Data Quotation</a>
+        <button
+          onClick={() => setShowModal(true)}
+          className="btn gap-2 btn-outline rounded-full btn-sm px-5 bg-white hover:bg-gray-100 text-[#C1C0BF]"
+        >
+          Tambah Data |
+          <BsPlusSquare />
+        </button>
+      </div>
+      <div className="relative bg-white shadow-xl m-6 mt-3 md:max-xl:flex ring-1 ring-gray-900/5">
+        <div className="p-4 bg-kuning border-b border-gray-200"></div>
+        <div className="bg-white border-b border-gray-200">
+          <div className="">
+            <AreaWisata area={areaList} />
+            <div className="m-2 flex justify-between items-center">
+              <a className="text-[10px] text-black">Showing 1 - 4 of 10</a>
+              <Pagination />
+            </div>
+          </div>
         </div>
-  )
+      </div>
+
+      <ModalArea onClose={handleOnClose} visible={showModal} />
+    </div>
+  );
 }
--- resources/js/Pages/Quotation/Quotations.jsx
+++ resources/js/Pages/Quotation/Quotations.jsx
@@ -9,14 +9,11 @@ import { BsPlusSquare } from "react-icons/bs";
 import ModalCrew from "@/Components/Item Quotation/Crew/ModalCrew";
 import Layout from "@/Layouts/Layout";
 
-export default function Quotations(props, crewL) {
+export default function Quotation(props, crewL) {
   // const {data} = this.props.location;
   console.log("CrewL", crewL);
   return (
     <div className="min-h-screen bg-abu">
-      {/* Nabvar */}
-      <Navbar user={props.auth.user} />
-
       {/* Content */}
       <div className="ml-6">
         <a>Quotation Manual</a>
@@ -29,24 +26,17 @@ export default function Quotations(props, crewL) {
         <div className="bg-white border-b border-gray-200">
           <div className="flex flex-col p-6">
             <a className="mr-5 mt-2 text-black font-bold mb-4">Quotation</a>
-            <div className="flex flex-row">
-              <ul className="text-black font-medium space-y-8">
-              <input
-                  type="text"
-                  className="input input-bordered w-1/2 bg-inherit shadow-md"
-                />
-                <li>Nama Klien</li>
-                <li>Paket</li>
-                <li>Jumlah Orang</li>
-                <li>Hari</li>
-                <li>Area Wisata</li>
-              </ul>
-              <div className=" flex flex-col space-y-2">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Nama Klien</span>
                 <input
                   type="text"
-                  className="input input-bordered w-full bg-inherit shadow-md"
+                  className="input input-bordered  bg-inherit shadow-md " 
                 />
-                <select className="select w-full max-w-xs bg-inherit shadow-md">
+              </label>
+              <label className="label">
+                <span className="label-text">Paket</span>
+                <select className="select  bg-inherit shadow-md ">
                   <option disabled selected>
                     Pilih Paket
                   </option>
@@ -56,17 +46,26 @@ export default function Quotations(props, crewL) {
                   <option>Lisa</option>
                   <option>Maggie</option>
                 </select>
+              </label>
+              <label className="label">
+                <span className="label-text">Jumlah Orang</span>
                 <input
                   type="text"
-                  className="input input-bordered max-w-xs bg-inherit shadow-md"
+                  className="input input-bordered bg-inherit shadow-md"
                 />
+              </label>
+              <label className="label">
+                <span className="label-text">Hari</span>
                 <input
                   type="text"
-                  className="input input-bordered max-w-xs bg-inherit shadow-md"
+                  className="input input-bordered bg-inherit shadow-md "
                 />
-                <select className="select w-full max-w-xs bg-inherit shadow-md">
+              </label>
+              <label className="label">
+                <span className="label-text">Paket</span>
+                <select className="select  bg-inherit shadow-md">
                   <option disabled selected>
-                    Pilih Area Wisata
+                    Pilih Paket
                   </option>
                   <option>Homer</option>
                   <option>Marge</option>
@@ -74,14 +73,36 @@ export default function Quotations(props, crewL) {
                   <option>Lisa</option>
                   <option>Maggie</option>
                 </select>
-                <button className="btn btn-accent">Button</button>
-              </div>
+              </label>
             </div>
+            <button className="btn btn-accent">Button</button>
+
+            {/* <ul className="text-black font-medium space-y-8">
+              <li>Nama Klien</li>
+              <li>Paket</li>
+              <li>Jumlah Orang</li>
+              <li>Hari</li>
+              <li>Area Wisata</li>
+            </ul> */}
           </div>
+          {/* <div className="flex flex-row">
+            <div className="flex flex-col">
+             
+              
+              <input
+                type="text"
+                className="input input-bordered max-w-xs bg-inherit shadow-md"
+              />
+              <input
+                type="text"
+                className="input input-bordered max-w-xs bg-inherit shadow-md"
+              />
+              
+            
+            </div>
+          </div> */}
         </div>
       </div>
     </div>
   );
 }
-
-Quotations.layout = page => <Layout children={page}/>
