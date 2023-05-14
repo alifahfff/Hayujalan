36216a0fdaa744482acab9e269cf196140ba6502
Yoga Nizar <nizaryhabib@gmail.com>
2023-05-14 08:26:28 +0700

    Update Manual Quotation Pages

--- resources/js/Pages/Quotation/Quotations.jsx
+++ resources/js/Pages/Quotation/Quotations.jsx
@@ -13,7 +13,7 @@ export default function Quotations(props, crewL) {
   // const {data} = this.props.location;
   console.log("CrewL", crewL);
   return (
-    <div className="min-h-screen bg-abu">
+    <div className="min-h-screen bg-abu ">
       {/* Content */}
       <div className="ml-6">
         <a>Quotation Manual</a>
@@ -24,88 +24,71 @@ export default function Quotations(props, crewL) {
       <div className="relative bg-white shadow-xl m-6 mt-3 md:max-xl:flex ring-1 ring-gray-900/5">
         <div className="p-4 bg-kuning border-b border-gray-200"></div>
         <div className="bg-white border-b border-gray-200">
-          <div className="flex flex-col p-6">
+          <div className="flex flex-col px-11 pt-6 pb-8 ">
             <a className="mr-5 mt-2 text-black font-bold mb-4">Quotation</a>
-            <div className="form-control">
+            <form className="max-w-3xl space-y-6 ">
               <label className="label">
-                <span className="label-text">Nama Klien</span>
+                <span className="label-text text-black">Nama Klien</span>
                 <input
                   type="text"
-                  className="input input-bordered  bg-inherit shadow-md " 
+                  className="input border-2 border-inherit  bg-inherit  w-2/4 mr-52"
                 />
               </label>
-              <label className="label">
-                <span className="label-text">Paket</span>
-                <select className="select  bg-inherit shadow-md ">
-                  <option disabled selected>
-                    Pilih Paket
-                  </option>
-                  <option>Homer</option>
-                  <option>Marge</option>
-                  <option>Bart</option>
-                  <option>Lisa</option>
-                  <option>Maggie</option>
-                </select>
-              </label>
-              <label className="label">
-                <span className="label-text">Jumlah Orang</span>
-                <input
-                  type="text"
-                  className="input input-bordered bg-inherit shadow-md"
-                />
-              </label>
-              <label className="label">
-                <span className="label-text">Hari</span>
-                <input
-                  type="text"
-                  className="input input-bordered bg-inherit shadow-md "
-                />
-              </label>
-              <label className="label">
-                <span className="label-text">Paket</span>
-                <select className="select  bg-inherit shadow-md">
-                  <option disabled selected>
-                    Pilih Paket
-                  </option>
-                  <option>Homer</option>
-                  <option>Marge</option>
-                  <option>Bart</option>
-                  <option>Lisa</option>
-                  <option>Maggie</option>
-                </select>
-              </label>
-            </div>
-            <button className="btn btn-accent">Button</button>
 
-            {/* <ul className="text-black font-medium space-y-8">
-              <li>Nama Klien</li>
-              <li>Paket</li>
-              <li>Jumlah Orang</li>
-              <li>Hari</li>
-              <li>Area Wisata</li>
-            </ul> */}
-          </div>
-          {/* <div className="flex flex-row">
-            <div className="flex flex-col">
-             
-              
-              <input
-                type="text"
-                className="input input-bordered max-w-xs bg-inherit shadow-md"
-              />
-              <input
-                type="text"
-                className="input input-bordered max-w-xs bg-inherit shadow-md"
-              />
-              
-            
+              <label className="label">
+                <span className="label-text  text-black">Paket</span>
+                <select className="select  bg-inherit border-2 border-inherit w-2/4 mr-52">
+                  <option disabled selected>
+                    Pilih Paket
+                  </option>
+                  <option>Homer</option>
+                  <option>Marge</option>
+                  <option>Bart</option>
+                  <option>Lisa</option>
+                  <option>Maggie</option>
+                </select>
+              </label>
+
+              <label className="label">
+                <span className="label-text  text-black">Jumlah Orang</span>
+                <input
+                  type="text"
+                  className="input border-2 border-inherit bg-inherit  w-2/4 mr-52"
+                />
+              </label>
+
+              <label className="label">
+                <span className="label-text  text-black">Hari</span>
+                <input
+                  type="text"
+                  className="input border-2 border-inherit bg-inherit  w-2/4 mr-52"
+                />
+              </label>
+
+              <label className="label">
+                <span className="label-text  text-black ">Area Wisata</span>
+                <select className="select  bg-inherit border-2 border-inherit w-2/4 mr-52">
+                  <option disabled selected>
+                    Pilih Paket
+                  </option>
+                  <option>Homer</option>
+                  <option>Marge</option>
+                  <option>Bart</option>
+                  <option>Lisa</option>
+                  <option>Maggie</option>
+                </select>
+              </label>
+            </form>
+            <div>
+              <button className="btn bg-green-600 text-white border-0 btn-md w-1/6 h-0 float-right">
+                submit
+              </button>
             </div>
-          </div> */}
+          </div>
         </div>
       </div>
     </div>
   );
 }
 
-
-Quotations.layout = page => <Layout children={page}/>
+Quotations.layout = (page) => <Layout children={page} />;
