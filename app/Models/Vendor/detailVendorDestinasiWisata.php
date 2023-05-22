415bab9d964b960dbf200b217c46fdeabd0c5275
novakensa29 <nova.kensa.tif20@polban.ac.id>
2023-05-22 19:04:25 +0700

    Destinasi Wisata

--- app/Models/Vendor/detailVendorDestinasiWisata.php
+++ app/Models/Vendor/detailVendorDestinasiWisata.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class detailVendorDestinasiWisata extends Model
 {
     use HasFactory;
+
+    protected $table = "detail_vendor_destinasi_wisatas";
+    protected $primaryKey = "id";
+
+    public function vendorDW()
+    {
+        return $this->hasMany(detailVendorDestinasiWisata::class, 'idDestinasiWisata', 'id');
+    }
 }
